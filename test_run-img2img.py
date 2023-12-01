6c75700b412d5bfa2b32112ab8a3b6ed922660c1
Strawberries <crowles98@gmail.com>
2023-12-01 15:07:27 +0000

    img2imglock tests

--- test_run-img2img.py
+++ test_run-img2img.py
@@ -1,10 +1,10 @@
 import torch
 
-from diffusers import StableVideoDiffusionPipeline
+from pipeline_stable_video_diffusion_img2img import StableVideoDiffusionPipelineImg2Img
 from diffusers.utils import load_image, export_to_video
 
 
-pipe = StableVideoDiffusionPipeline.from_pretrained(
+pipe = StableVideoDiffusionPipelineImg2Img.from_pretrained(
     "stabilityai/stable-video-diffusion-img2vid"
 )
 pipe.enable_model_cpu_offload()
@@ -14,6 +14,6 @@ image = load_image("https://huggingface.co/datasets/huggingface/documentation-im
 image = image.resize((1024, 576))
 
 generator = torch.manual_seed(42)
-frames = pipe(image, decode_chunk_size=8, generator=generator).frames[0]
+frames = pipe(image, decode_chunk_size=8, generator=generator,end_frame=image).frames[0]
 
 export_to_video(frames, "generated.mp4", fps=7)
