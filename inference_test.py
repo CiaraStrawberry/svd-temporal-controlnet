dbc33b2bd3a05bf401a7070149901d509eaeab5d
Strawberries <crowles98@gmail.com>
2023-12-21 15:24:49 +0000

    assorted inference fixes

--- inference_test.py
+++ inference_test.py
@@ -6,6 +6,7 @@ from PIL import Image
 from pipeline_stable_video_diffusion_controlnet import StableVideoDiffusionPipelineControlNet
 from controlnet_sdv import ControlNetSDVModel
 from unet_spatio_temporal_condition_controlnet import UNetSpatioTemporalConditionControlNetModel
+import cv2
 
 # Define functions
 def validate_and_convert_image(image, target_size=(256, 256)):
@@ -48,9 +49,28 @@ def create_image_grid(images, rows, cols, target_size=(256, 256)):
 
     return grid
 
+def tensor_to_pil(tensor):
+    """ Convert a PyTorch tensor to a PIL Image. """
+    # Convert tensor to numpy array
+    if len(tensor.shape) == 4:  # batch of images
+        images = [Image.fromarray(img.numpy().transpose(1, 2, 0)) for img in tensor]
+    else:  # single image
+        images = Image.fromarray(tensor.numpy().transpose(1, 2, 0))
+    return images
+
 def save_combined_frames(batch_output, validation_images, validation_control_images, output_folder):
     # Flatten batch_output to a list of PIL Images
     flattened_batch_output = [img for sublist in batch_output for img in sublist]
+
+    # Convert tensors in lists to PIL Images
+    validation_images = [tensor_to_pil(img) if torch.is_tensor(img) else img for img in validation_images]
+    validation_control_images = [tensor_to_pil(img) if torch.is_tensor(img) else img for img in validation_control_images]
+    flattened_batch_output = [tensor_to_pil(img) if torch.is_tensor(img) else img for img in flattened_batch_output]
+
+    # Flatten lists if they contain sublists (for tensors converted to multiple images)
+    validation_images = [img for sublist in validation_images for img in (sublist if isinstance(sublist, list) else [sublist])]
+    validation_control_images = [img for sublist in validation_control_images for img in (sublist if isinstance(sublist, list) else [sublist])]
+    flattened_batch_output = [img for sublist in flattened_batch_output for img in (sublist if isinstance(sublist, list) else [sublist])]
 
     # Combine frames into a list
     combined_frames = validation_images + validation_control_images + flattened_batch_output
@@ -72,35 +92,93 @@ def save_combined_frames(batch_output, validation_images, validation_control_ima
 
 def load_images_from_folder(folder):
     images = []
-    valid_extensions = {".jpg", ".jpeg", ".png", ".bmp", ".gif", ".tiff"}
+    valid_extensions = {".jpg", ".jpeg", ".png", ".bmp", ".gif", ".tiff"}  # Add or remove extensions as needed
 
-    for filename in sorted(os.listdir(folder)):
+    # Function to extract frame number from the filename
+    def frame_number(filename):
+        parts = filename.split('_')
+        if len(parts) > 1 and parts[0] == 'frame':
+            try:
+                return int(parts[1].split('.')[0])  # Extracting the number part
+            except ValueError:
+                return float('inf')  # In case of non-integer part, place this file at the end
+        return float('inf')  # Non-frame files are placed at the end
+
+    # Sorting files based on frame number
+    sorted_files = sorted(os.listdir(folder), key=frame_number)
+
+    # Load images in sorted order
+    for filename in sorted_files:
         ext = os.path.splitext(filename)[1].lower()
         if ext in valid_extensions:
-            img_path = os.path.join(folder, filename)
-            img = Image.open(img_path).convert('RGB')
+            img = Image.open(os.path.join(folder, filename)).convert('RGB')
             images.append(img)
 
     return images
+
+def load_images_from_folder_to_pil(folder, target_size=(512, 512)):
+    images = []
+    valid_extensions = {".jpg", ".jpeg", ".png", ".bmp", ".gif", ".tiff"}  # Add or remove extensions as needed
+
+    def frame_number(filename):
+        parts = filename.split('_')
+        if len(parts) > 1 and parts[0] == 'frame':
+            try:
+                return int(parts[1].split('.')[0])  # Extracting the number part
+            except ValueError:
+                return float('inf')  # In case of non-integer part, place this file at the end
+        return float('inf')  # Non-frame files are placed at the end
+
+    # Sorting files based on frame number
+    sorted_files = sorted(os.listdir(folder), key=frame_number)
+
+    # Load, resize, and convert images
+    for filename in sorted_files:
+        ext = os.path.splitext(filename)[1].lower()
+        if ext in valid_extensions:
+            img_path = os.path.join(folder, filename)
+            img = cv2.imread(img_path, cv2.IMREAD_UNCHANGED)  # Read image with original channels
+            if img is not None:
+                # Resize image
+                img = cv2.resize(img, target_size, interpolation=cv2.INTER_AREA)
+
+                # Convert to uint8 if necessary
+                if img.dtype == np.uint16:
+                    img = (img / 256).astype(np.uint8)
+
+                # Ensure all images are in RGB format
+                if len(img.shape) == 2:  # Grayscale image
+                    img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
+                elif len(img.shape) == 3 and img.shape[2] == 3:  # Color image in BGR format
+                    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+                # Convert the numpy array to a PIL image
+                pil_img = Image.fromarray(img)
+                images.append(pil_img)
+
+    return images
+
+# Usage example
 
 # Main script
 if __name__ == "__main__":
     args = {
         "pretrained_model_name_or_path": "stabilityai/stable-video-diffusion-img2vid",
-        "validation_image_folder": "./run_tests/rgb",
-        "validation_control_folder": "./run_tests/depth",
-        "validation_image": "./run_tests/dancer.png",
+        "validation_image_folder": "./run_tests/3/rgb",
+        "validation_control_folder": "./run_tests/3/depth",
+        "validation_image": "./run_tests/3/scene.png",
         "output_dir": "./output",
-        "height": 256,
-        "width": 256,
+        "height": 512,
+        "width": 512,
         # Additional arguments can be added here
     }
 
     # Load validation images and control images
-    validation_images = load_images_from_folder(args["validation_image_folder"])
-    validation_control_images = load_images_from_folder(args["validation_control_folder"])
+    validation_images = load_images_from_folder_to_pil(args["validation_image_folder"])
+    validation_control_images = load_images_from_folder_to_pil(args["validation_control_folder"])
     validation_image = Image.open(args["validation_image"]).convert('RGB')
-    
+
+
     # Load and set up the pipeline
     controlnet = controlnet = ControlNetSDVModel.from_pretrained("./controlnet")
     unet = UNetSpatioTemporalConditionControlNetModel.from_pretrained(args["pretrained_model_name_or_path"],subfolder="unet")
@@ -114,7 +192,7 @@ if __name__ == "__main__":
 
     # Inference and saving loop
 
-    video_frames = pipeline(validation_images[0], validation_control_images[:14], decode_chunk_size=8,num_frames=14)
+    video_frames = pipeline(validation_image, validation_control_images[:14], decode_chunk_size=8,num_frames=14).frames
 
     save_combined_frames(video_frames,validation_images, validation_control_images,val_save_dir)
     #save_combined_frames(video_frames, validation_images, validation_control_images,val_save_dir)
